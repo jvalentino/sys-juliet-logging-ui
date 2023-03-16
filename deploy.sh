56a778bb9a1c43789c6290e3536ab87ed9ad3a6d
John Valentino <jvalentino2@gmail.com>
2023-03-16 15:56:13 -0500

    initial port

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+kubectl delete deploy kibana-deployment || true
+kubectl delete service kibana || true
+kubectl delete configmaps kibana-config || true
+kubectl create -f ./config/helm/kibana/deployment.yaml
+
+kubectl wait pods -l app=kibana --for condition=Ready
+
+sh -x ./verify.sh
+
+sh -x ./import.sh
